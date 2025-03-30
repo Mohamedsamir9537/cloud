038f249b1f12ded5cdb146ec79e6e002d0017b79
Mohamed_Samir <110284523+Mohamedsamir9537@users.noreply.github.com>
2025-03-30 14:09:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,57 +1,16 @@
-const { readFileSync } = require("fs");
-const http = require("http");
-
-const homepage = readFileSync('./navbar-app/index.html')
-const homestyle = readFileSync('./navbar-app/styles.css')
-const homeimage = readFileSync('./navbar-app/logo.svg')
-const homlogic = readFileSync('./navbar-app/browser-app.js')
-
-
-const server = http.createServer((req,res)=>{
-    const url = req.url
-    if(url==='/'){console.log(req.url);
-    res.writeHead(200,{'content-type':'text/html'})
-    res.write(homepage)
-    }
-    else if(url==='/styles.css'){
-        res.writeHead(200,{'content-type':'text/css'})
-            res.write(homestyle)
-            res.end()
-            
-    }
-    else if(url==='/logo.svg'){
-        res.writeHead(200,{'content-type':'image/svg+xml'})
-            res.write(homeimage)
-            res.end()
-            
-    }
-    else if(url==='/browser-app.js'){
-        res.writeHead(200,{'content-type':'text/javascript'})
-            res.write(homlogic)
-            res.end()
-            
-    }
-
-            
-    
-   res.end()
-    
-});
+const express = require('express')
+const app =express()
+app.use(express.static('./methods-public'))
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html '))
+})
+app.all('*',(req,res)=>{
+    res.status(404).send(" resourse not found");
+})
 
 
 
 
-
-server.listen(5000)
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(5000,()=>{
+    console.log(" i am listening on port 5000")
+})
